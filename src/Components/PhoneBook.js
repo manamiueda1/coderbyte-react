3346e47e5372f3d17db9c2465ece4d887ebaeb79
Manami Ueda <manamiueda1@gmail.com>
2021-02-28 19:39:37 -0500

    commentedout

--- src/Components/PhoneBook.js
+++ src/Components/PhoneBook.js
@@ -1,6 +1,6 @@
-import { findByLabelText } from "@testing-library/react";
+// import { findByLabelText } from "@testing-library/react";
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 
 const style = {
   table: {
